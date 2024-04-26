f35a3097768e2ed1cdba9b3f57c8bf2db6abd41c
jovenebio <jovenebio@gmail.com>
2024-04-26 17:32:49 -0500

    updated footer year

--- src/JS/footer/footer.js
+++ src/JS/footer/footer.js
@@ -2,7 +2,7 @@ function FOOTER_CONTENT() {
     return (
         <footer>
             <div class="footer_content">
-                <h3>© Joven Ebio 2023, Powered by Amazon Web Services</h3>
+                <h3>© Joven Ebio 2024, Powered by Amazon Web Services</h3>
             </div>
         </footer>
     );
